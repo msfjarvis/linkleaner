910b7f59ae81fb07f8e88cb76b26463aaba71de5
Harsh Shandilya <me@msfjarvis.dev>
2020-02-22 20:13:28 +0530

    utils: Restrict visibility
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,8 @@
-pub fn escape_markdown_str(msg: String) -> String {
+pub(crate) fn escape_markdown_str(msg: String) -> String {
     msg.replace("_", r"\_")
 }
 
-pub fn join_results_to_string(items: Vec<String>, base_url: &str) -> String {
+pub(crate) fn join_results_to_string(items: Vec<String>, base_url: &str) -> String {
     let mut ret = String::new();
     for item in items.iter() {
         ret.push_str(&format!("[{}]({}/{})\n", item, base_url, item));

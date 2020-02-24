ecc76d11edd37987f347582821cd72f116176d32
Harsh Shandilya <me@msfjarvis.dev>
2020-02-24 10:46:04 +0530

    Implement /pic
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use rand::{thread_rng, Rng};
 use std::env;
 use walkdir::WalkDir;
 
+use teloxide::requests::SendPhoto;
 use utils::{file_name_to_label, join_results_to_string};
 
 lazy_static! {
@@ -22,17 +23,38 @@ lazy_static! {
 enum Command {
     #[command(description = "display this text.")]
     Help,
+    #[command(description = "return a picture matching a given query")]
+    Pic,
     #[command(description = "return a random picture")]
     Random,
     #[command(description = "search picture based on given string")]
     Search,
 }
 
-fn get_random_file() -> String {
-    FILES
-        .get(thread_rng().gen_range(0, *FILE_COUNT))
+fn get_random_file(files: Vec<String>) -> String {
+    files
+        .get(thread_rng().gen_range(0, files.len()))
         .unwrap()
+        .to_string()
+}
+
+fn get_search_results(search_term: &str) -> Vec<String> {
+    FILES
         .clone()
+        .into_iter()
+        .filter(|x| x.starts_with(search_term))
+        .collect()
+}
+
+fn send_captioned_picture(
+    cx: DispatcherHandlerCx<Message>,
+    link: String,
+    file: String,
+) -> SendPhoto {
+    cx.answer_photo(InputFile::url(&link))
+        .caption(format!("[{}]({})", &file_name_to_label(file), link))
+        .parse_mode(ParseMode::MarkdownV2)
+        .reply_to_message_id(cx.update.id)
 }
 
 async fn answer(
@@ -47,23 +69,27 @@ async fn answer(
                 .send()
                 .await?
         }
+        Command::Pic => {
+            if args.is_empty() {
+                cx.answer("No search query passed")
+                    .reply_to_message_id(cx.update.id)
+                    .send()
+                    .await?
+            } else {
+                let results = get_search_results(&args.join("_"));
+                let file = get_random_file(results);
+                let link = format!("{}/{}", *BASE_URL, file);
+                send_captioned_picture(cx, link, file).send().await?
+            }
+        }
         Command::Random => {
-            let file = get_random_file();
+            let file = get_random_file((*FILES).clone());
             let link = format!("{}/{}", *BASE_URL, file);
-            cx.answer_photo(InputFile::url(&link))
-                .caption(format!("[{}]({})", &file_name_to_label(file), link))
-                .parse_mode(ParseMode::MarkdownV2)
-                .reply_to_message_id(cx.update.id)
-                .send()
-                .await?
+            send_captioned_picture(cx, link, file).send().await?
         }
         Command::Search => {
             let search_term = args.join("_");
-            let res: Vec<String> = FILES
-                .clone()
-                .into_iter()
-                .filter(|x| x.starts_with(&search_term))
-                .collect();
+            let res = get_search_results(&search_term);
             cx.answer(join_results_to_string(search_term, res, &**BASE_URL))
                 .parse_mode(ParseMode::MarkdownV2)
                 .disable_web_page_preview(true)

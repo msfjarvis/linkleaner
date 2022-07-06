ead99a8278b2c175f6da8783d8a919827e50a241
Harsh Shandilya <me@msfjarvis.dev>
2022-07-06 12:15:13 +0530

    feat: implement twitter -> vxtwitter auto-replace

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 mod commands;
 mod utils;
 
-use std::{env, error::Error, path::PathBuf};
+use std::{env, error::Error, marker::Send, path::PathBuf};
 
 use dotenv::dotenv;
 use once_cell::sync::Lazy;
+use regex::Regex;
 use teloxide::{
     adaptors::auto_send::AutoRequest,
     payloads::{SendDocument, SendPhoto},
@@ -28,6 +29,8 @@ static BASE_DIR: Lazy<String> =
     Lazy::new(|| env::var("BASE_DIR").expect("BASE_DIR must be defined"));
 static TREE: Lazy<sled::Db> = Lazy::new(|| sled::open("file_id_cache").unwrap());
 static FILES: Lazy<Vec<String>> = Lazy::new(|| index_pictures(&BASE_DIR));
+static TWITTER_REGEX: Lazy<Regex> =
+    Lazy::new(|| Regex::new("https://twitter.com/.*/status/.*").unwrap());
 
 /// Telegram mandates a photo can not be larger than 10 megabytes
 const MAX_FILE_SIZE: u64 = 10_485_760;
@@ -231,6 +234,30 @@ async fn answer(
     Ok(())
 }
 
+async fn vxtwitter_handler(
+    bot: AutoSend<Bot>,
+    message: Message,
+) -> Result<(), Box<dyn Error + Sync + Send + 'static>> {
+    if let Some(text) = message.text() {
+        if let Some(user) = message.from() {
+            if TWITTER_REGEX.is_match(text) {
+                let text = text.replace("https://twitter.com", "https://vxtwitter.com");
+                let text = format!(
+                    "<a href=\"{}\">{}</a>: {}",
+                    user.id.url(),
+                    user.full_name(),
+                    text
+                );
+                bot.delete_message(message.chat.id, message.id).await?;
+                bot.send_message(message.chat.id, text)
+                    .parse_mode(ParseMode::Html)
+                    .await?;
+            }
+        }
+    }
+    Ok(())
+}
+
 async fn run() {
     #[cfg(feature = "console")]
     console_subscriber::init();
@@ -239,7 +266,20 @@ async fn run() {
     log::debug!("Indexed {} files", FILES.len());
 
     let bot = Bot::from_env().auto_send();
-    teloxide::commands_repl(bot, answer, Command::ty()).await;
+
+    let handler = dptree::entry()
+        .branch(
+            Update::filter_message()
+                .filter_command::<Command>()
+                .endpoint(answer),
+        )
+        .branch(Update::filter_message().endpoint(vxtwitter_handler));
+
+    Dispatcher::builder(bot, handler)
+        .build()
+        .setup_ctrlc_handler()
+        .dispatch()
+        .await;
 }
 
 #[tokio::main]

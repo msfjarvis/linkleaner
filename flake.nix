7cbf6b97549b923fa1b290cf4a91bded154bcbaf
Harsh Shandilya <me@msfjarvis.dev>
2023-05-15 11:16:45 +0530

    feat(nix): switch to provisioning toolchains using fenix
    rust-overlay's `fromRustupToolchainFile` does not provision the provided
    components from the `rust-toolchain.toml` file which makes the experience
    subpar compared to fenix that treats `rust-toolchain.toml` similar to rustup.

--- flake.nix
+++ flake.nix
@@ -4,19 +4,18 @@
   inputs = {
     nixpkgs = {url = "github:NixOS/nixpkgs/nixpkgs-unstable";};
 
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+
     flake-utils = {url = "github:numtide/flake-utils";};
 
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
-    };
-
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-      };
     };
 
     crane = {
@@ -25,7 +24,6 @@
         flake-compat.follows = "flake-compat";
         flake-utils.follows = "flake-utils";
         nixpkgs.follows = "nixpkgs";
-        rust-overlay.follows = "rust-overlay";
       };
     };
 
@@ -38,19 +36,20 @@
   outputs = {
     self,
     nixpkgs,
+    fenix,
     crane,
     flake-utils,
     advisory-db,
-    rust-overlay,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [(import rust-overlay)];
+      pkgs = import nixpkgs {inherit system;};
+
+      rustNightly = (import fenix {inherit pkgs;}).fromToolchainFile {
+        file = ./rust-toolchain.toml;
+        sha256 = "sha256-1elQHBWEQRZ5qrEtJi6uhvwUNKedyQusyhATdBywep0=";
       };
 
-      rustNightly = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
       craneLib = (crane.mkLib pkgs).overrideToolchain rustNightly;
       commonArgs = {
         src = craneLib.cleanCargoSource ./.;

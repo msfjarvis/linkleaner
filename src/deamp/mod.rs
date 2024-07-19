080d4f4bfcafdba4e89a6dc86a3a9f29ff51e147
Harsh Shandilya <me@msfjarvis.dev>
2024-07-19 15:20:05 +0530

    fix(deamp): detect amp in query params

--- src/deamp/mod.rs
+++ src/deamp/mod.rs
@@ -46,7 +46,9 @@ pub fn is_amp(url: &Url) -> bool {
     if let Some(mut segments) = url.path_segments()
         && let Some(host) = url.host_str()
     {
-        segments.any(|x| x == "amp") || host.ends_with(".cdn.ampproject.org")
+        segments.any(|x| x == "amp")
+            || host.ends_with(".cdn.ampproject.org")
+            || url.query().map_or(false, |query| query.contains("amp"))
     } else {
         false
     }
@@ -56,10 +58,11 @@ pub fn is_amp(url: &Url) -> bool {
 mod tests {
     use super::is_amp;
     use reqwest::Url;
-    const DATA: [(bool, &str); 3] = [
+    const DATA: [(bool, &str); 4] = [
         (true, "https://www.google.com/amp/s/m.gsmarena.com/samsung_galaxy_tab_s9-ampp-12439.php"),
         (true, "https://www.google.com/amp/s/news.abplive.com/news/india/microsoft-server-outage-multiple-airports-hit-by-web-check-in-server-glitch-1703909/amp"),
         (false, "https://github.com"),
+        (true, "https://josysnavi.jp/2024/blog-00058?amp"),
     ];
 
     #[test]

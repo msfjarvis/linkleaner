910b1ca9d828ce41c38b53eaa1a338bfb392d055
Harsh Shandilya <me@msfjarvis.dev>
2024-08-22 17:54:57 +0530

    WIP: refactor: port Instagram fixer to LinkPreviewOptions

--- src/instagram.rs
+++ src/instagram.rs
@@ -1,56 +1,46 @@
-use crate::{
-    message::BotExt,
-    utils::{scrub_urls, AsyncError},
+use crate::utils::{get_urls_from_message, AsyncError};
+use teloxide::{
+    payloads::SendMessageSetters,
+    prelude::Requester,
+    types::{LinkPreviewOptions, Message, ParseMode, ReplyParameters},
+    utils::html::link,
+    Bot,
 };
-use regex::Regex;
-use std::sync::LazyLock;
-use teloxide::{types::Message, utils::html::link, Bot};
-
-const HOST_MATCH_GROUP: &str = "host";
-
+use tracing::trace;
 pub const DOMAINS: [&str; 1] = ["instagram.com"];
-static MATCH_REGEX: LazyLock<Regex> = LazyLock::new(|| {
-    Regex::new("https://(?:www.)?(?P<host>instagram.com)/(.*/)?(p|reel|tv)/[A-Za-z0-9]+.*/")
-        .unwrap()
-});
 
 pub async fn handler(bot: Bot, message: Message) -> Result<(), AsyncError> {
-    if let Some(text) = scrub_urls(&message)
+    if let Some(text) = &message.text()
         && let Some(ref user) = message.from
-        && let Some(caps) = MATCH_REGEX.captures(&text)
     {
-        let text = text.replace(&caps[HOST_MATCH_GROUP], "ddinstagram.com");
         let text = format!("{}: {}", link(user.url().as_str(), &user.full_name()), text);
-        bot.replace_chat_message(&message, &text).await?;
-    }
-    Ok(())
-}
-
-#[cfg(test)]
-mod test {
-    use super::{HOST_MATCH_GROUP, MATCH_REGEX};
-
-    // https://regex101.com/r/pOizaT/1
-    #[test]
-    fn verify_regex() {
-        let items = vec![
-            "https://www.instagram.com/p/CgJESh6hxsS/",
-            "https://instagram.com/p/CgJESh6hxsS/",
-            "https://www.instagram.com/reel/CgHIG0Ih3XF/",
-            "https://www.instagram.com/tv/CgHIG0Ih3XF/",
-            "https://www.instagram.com/zuck/reel/C9AbmIYp3Js/",
-        ];
-        for item in items {
-            assert!(MATCH_REGEX.is_match(item), "{item} failed to match");
-            assert!(
-                MATCH_REGEX.is_match(&format!("Some leading text {item}")),
-                "{item} failed to match"
-            );
+        let url: Option<String> = get_urls_from_message(&message)
+            .first()
+            .map(|url| String::from(url.clone()).replace("instagram.com", "ddinstagram.com"));
+        trace!(?url);
+        let preview_options = LinkPreviewOptions {
+            is_disabled: false,
+            url,
+            prefer_small_media: false,
+            prefer_large_media: true,
+            show_above_text: true,
+        };
+        let _del = bot.delete_message(message.chat.id, message.id).await;
+        if let Some(reply) = message.reply_to_message() {
+            bot.send_message(message.chat.id, text)
+                .parse_mode(ParseMode::Html)
+                .reply_parameters(ReplyParameters::new(reply.id))
+                .link_preview_options(preview_options)
+                .await?;
+            Ok(())
+        } else {
+            bot.send_message(message.chat.id, text)
+                .parse_mode(ParseMode::Html)
+                .link_preview_options(preview_options)
+                .await?;
+            Ok(())
         }
-        assert!(!MATCH_REGEX.is_match("https://www.instagram.com/starsmitten_/"));
-        let caps = MATCH_REGEX
-            .captures("https://www.instagram.com/p/CfZdFVUJyQG/")
-            .unwrap();
-        assert_eq!(&caps[HOST_MATCH_GROUP], "instagram.com");
+    } else {
+        Ok(())
     }
 }

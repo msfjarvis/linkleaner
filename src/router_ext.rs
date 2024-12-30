13151a226a99a7553493176bbb9b6d9cffd8ce25
Harsh Shandilya <me@msfjarvis.dev>
2024-12-30 18:25:47 +0530

    fix: panic if `add_route` gets a path with a trailing slash
    This will get surfaced by tests

--- src/router_ext.rs
+++ src/router_ext.rs
@@ -1,6 +1,10 @@
 /// Naive macro to add a route and its trailing slash variant to the router
 macro_rules! add_route {
     ($router:ident, $route:literal) => {
+        assert!(
+            !$route.ends_with("/"),
+            "URLs passed to add_route must not end in /"
+        );
         $router.insert($route, ()).unwrap();
         $router.insert(concat!($route, "/"), ()).unwrap();
     };

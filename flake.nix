d119501d74922eb699e30b4214bf831085ae3bda
Harsh Shandilya <me@msfjarvis.dev>
2026-05-12 08:21:03 +0530

    fix: upgrade to Rust 1.95.0

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
 
         rustNightly = (import fenix { inherit pkgs; }).fromToolchainFile {
           file = ./rust-toolchain.toml;
-          sha256 = "sha256-vra6TkHITpwRyA5oBKAHSX0Mi6CBDNQD+ryPSpxFsfg=";
+          sha256 = "sha256-gh/xTkxKHL4eiRXzWv8KP7vfjSk61Iq48x47BEDFgfk=";
         };
 
         craneLib = (crane.mkLib pkgs).overrideToolchain rustNightly;

bf7c23c5a4b03a0a93564720806120b7d99a8dab
Harsh Shandilya <me@msfjarvis.dev>
2026-08-29 18:16:22 +0530

    fix(rust): update to v1.98.0

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
 
         rustNightly = (import fenix { inherit pkgs; }).fromToolchainFile {
           file = ./rust-toolchain.toml;
-          sha256 = "sha256-A1abGIbOtcBSdrUMhDGrER3pRM1hQP4fp9gh3Y4PKc8=";
+          sha256 = "sha256-P30Tm3O7vQAE725YtDCDHGjNrSsfZO4us11UwJGZSJo=";
         };
 
         craneLib = (crane.mkLib pkgs).overrideToolchain rustNightly;

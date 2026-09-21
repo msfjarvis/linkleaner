cce34fbd4356bcd5a5127144fd5b100869943532
Harsh Shandilya <me@msfjarvis.dev>
2026-09-21 15:18:00 +0530

    fix: delete legacy Nix shim and move to Nixpkgs tarball

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "linkleaner";
 
-  inputs.nixpkgs.url = "github:msfjarvis/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs.url = "https://nixexprs.msfjarvis.dev/nixexprs.tar.zst";
 
   inputs.systems.url = "github:msfjarvis/flake-systems";
 
